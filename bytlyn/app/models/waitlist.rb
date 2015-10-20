f62a3bbe08871c459caa0927e990d9e0c71f0f91
Jessica Lie <jenadlie@berkeley.edu>
2015-10-20 16:53:21 -0700

    showing waitlist for both restaurant and customer. worked fine under the assumption: rest id is a proper rest id one user can only waitlist on the same rest id once. need verification upon saving waitlist.

--- bytlyn/app/models/waitlist.rb
+++ bytlyn/app/models/waitlist.rb
@@ -1,2 +1,20 @@
 class Waitlist < ActiveRecord::Base
+
+
+    def self.get_restaurant_waitlist(user_id)
+        return Waitlist.where(rest_id: user_id).order(:created_at)
+    end
+
+    def self.get_customer_waitlist(user_id)
+        list_and_position = []
+        lists = Waitlist.where(cust_id: user_id).order(:created_at)
+        lists.each do |list|
+            this_list = get_restaurant_waitlist(list.rest_id)
+            pos = this_list.count(:conditions => ['created_at < ?', list.created_at])
+            list_and_position << {:list => list, :position => pos}
+        end
+        return list_and_position
+    end
+
+
 end
