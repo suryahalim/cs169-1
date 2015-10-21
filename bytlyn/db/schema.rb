7c9caba32aa08829de45696bb12cb8e1e769799f
Bernardus Billy Tjiptoning <bernardus.billy@hotmail.com>
2015-10-21 01:24:07 -0700

    Finished up profile page

--- bytlyn/db/schema.rb
+++ bytlyn/db/schema.rb
@@ -16,6 +16,15 @@ ActiveRecord::Schema.define(version: 20151009043556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "accounts", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.boolean  "rest",            null: false
+    t.string   "name",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
