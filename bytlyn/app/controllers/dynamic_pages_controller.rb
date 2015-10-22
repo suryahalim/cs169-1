38429f6116f720c498d0ece743b8c3801a09ab65
Bernardus Billy Tjiptoning <bernardus.billy@hotmail.com>
2015-10-21 17:35:32 -0700

    Merging frontend to backend : Put header and footer in application.html.erb.

--- bytlyn/app/controllers/dynamic_pages_controller.rb
+++ bytlyn/app/controllers/dynamic_pages_controller.rb
@@ -17,9 +17,7 @@ class DynamicPagesController < ApplicationController
 
     def home
         if user_signed_in?
- 
             redirect_to profile_path
-
         else
             redirect_to index_path
         end
