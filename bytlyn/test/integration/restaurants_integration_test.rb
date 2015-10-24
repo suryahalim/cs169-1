f25cd1dfbe8208e106d95714e3fbeddbc5a311bc
fendy93 <fsoegiono@berkeley.edu>
2015-10-23 19:07:21 -0700

    Restaurant Integration Test

---
+++ bytlyn/test/integration/restaurants_integration_test.rb
@@ -0,0 +1,76 @@
+require 'test_helper'
+ 
+class RestaurantIntegrationTest < ActionDispatch::IntegrationTest
+  test "create and login" do
+    https!
+    get "/login"
+    assert_response :success
+
+    #create user test
+    post_via_redirect create_user_path, 'user[name]' => 'FendyOnel', 'user[email]' => 'FendyOnel@gmail.com', 'user[password]' => '123123123', 'user[password_confirmation]' => '123123123'
+    assert_equal '/profile', path
+
+    #sign in test
+    post_via_redirect user_session_path, 'user[email]' => 'FendyOnel@gmail.com', 'user[password]' => '123123123'
+    assert_equal '/profile', path
+
+    #tear down
+    delete destroy_user_session_path
+    delete destroy_user_path
+  end
+
+  test "login waitlist restaurant already sign up" do
+    https?
+    get "/restaurants"
+    assert_response :success
+
+    get new_user_session_path, 'user[email]' => 'FendyOnel@gmail.com', 'user[password]' => '123123123'
+    get_via_redirect "/waitlists_new?rest_id=@user.id"
+    assert_equal '/waitlists_new', path
+
+    #tear down
+    delete destroy_user_session_path
+    delete destroy_user_path
+  end
+
+   test "login waitlist restaurant not yet sign in" do
+    https!
+    get "/restaurants"
+    assert_response :success
+
+    delete destroy_user_session_path
+    get_via_redirect waitlists_path
+    assert_equal '/login', path
+   end
+
+  test "create new restaurant" do
+    https!
+    get "/restaurants"
+    assert_response :success
+
+    #create user test
+    post_via_redirect create_restaurant_path, 'user[name]' => 'FendyBilly', 'user[email]' => 'FendyBilly@gmail.com', 'user[password]' => '123123123', 'user[password_confirmation]' => '123123123'
+    assert_equal '/profile', path
+
+    #sign in test
+    post_via_redirect user_session_path, 'user[email]' => 'FendyBilly@gmail.com', 'user[password]' => '123123123'
+    assert_equal '/profile', path
+
+    #tear down
+    delete destroy_user_session_path
+    delete destroy_user_path
+   end
+
+  test "count how many restaurants" do
+    #create user test
+    assert_difference('Restaurant.count', 1) do
+        post '/create-restaurant', 'user[name]' => 'Billy1', 'user[email]' => 'Billy1@gmail.com', 'user[password]' => '1235abcd', 'user[password_confirmation]' => '1235abcd'
+        get '/signup-restaurant'
+    end
+
+    #tear down
+    delete destroy_user_session_path
+    delete destroy_user_path
+   end
+
+end
