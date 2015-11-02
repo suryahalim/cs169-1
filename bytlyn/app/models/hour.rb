866ff6434a46b910f83bd359511e6767b3630f1c
Jessica Lie <jenadlie@berkeley.edu>
2015-11-01 22:51:35 -0800

    fix waitlist error

---
+++ bytlyn/app/models/hour.rb
@@ -0,0 +1,22 @@
+class Hour < ActiveRecord::Base
+    validates_uniqueness_of :rest_id, :scope => [:day_id]
+    validates :rest_id, presence: true
+    validates :day_id, presence: true
+    validate :validate_day_id
+    validate :validate_open_close_hours
+
+    def validate_day_id
+        errors.add(:day_id, "not within range") if day_id > 7 or day_id < 1
+    end
+    def validate_open_close_hours
+        if (open == nil && close != nil) or (close == nil && open != nil)
+            errors.add(:open, "hour nil, but close hour not nil or vice versa")
+        end
+        if open != nil && close != nil
+            errors.add(:open, "hour not within range") if open > 24 or open < 1
+            errors.add(:close, "hour not within range") if close > 24 or close < 1
+            errors.add(:open, "hour not before close hour") if open > close
+        end
+    end
+
+end
--- bytlyn/test/models/waitlist_test.rb
+++ bytlyn/test/models/waitlist_test.rb
@@ -30,17 +30,17 @@ class WaitlistTest < ActiveSupport::TestCase
     assert_equal(status, true)
   end
 
-  ## error should be catch outside db. don't let error reach db
-  # test "no cust id" do
-  #   @list = Waitlist.new(cust_id: "", rest_id: 1, people: 3)
-  #   @list.save
-  #   # assert_equal(status, false)
-  # end
-  # test "no rest id" do
-  #   @list = Waitlist.new(cust_id: 2, rest_id: "", people: 3)
-  #   status = @list.save
-  #   assert_equal(status, false)
-  # end
+  # error should be catch outside db. don't let error reach db
+  test "no cust id" do
+    @list = Waitlist.new(cust_id: "", rest_id: 1, people: 3)
+    @list.save
+    # assert_equal(status, false)
+  end
+  test "no rest id" do
+    @list = Waitlist.new(cust_id: 2, rest_id: "", people: 3)
+    status = @list.save
+    assert_equal(status, false)
+  end
 
   test "no people" do
     @list = Waitlist.new(cust_id: 2, rest_id: 1, people: "")
