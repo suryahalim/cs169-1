e22f6f3d6e43bb0bf42822f00253d6963b9ce179
Jenkins <jenkins@example.com>
2015-11-03 15:08:14 -0800

    Simplecov code coverage added

--- bytlyn/test/test_helper.rb
+++ bytlyn/test/test_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter
+]
+
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
