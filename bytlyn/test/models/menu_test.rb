650a20fc2ff80b246708b1266b83188bd2df937b
Cecilia Maharani <ceciliastrid.cia@gmail.com>
2015-11-06 21:57:10 -0800

    menus models test

--- bytlyn/test/models/menu_test.rb
+++ bytlyn/test/models/menu_test.rb
@@ -4,4 +4,52 @@ class MenuTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "successful new menu" do
+    @menu = Menu.new(rest_id:1, name: "test", description: "desc test", price: 10)
+    assert @menu.save
+  end
+  
+  test "not successful no rest id" do
+    @menu = Menu.new(name: "test", description: "desc test", price: 10)
+    assert_equal(@menu.save, false)
+  end
+
+  test "not successful no name" do
+    @menu = Menu.new(rest_id:1, description: "desc test", price: 10)
+    assert_equal(@menu.save, false)
+  end
+
+  test "not successful no price" do
+    @menu = Menu.new(rest_id:1, description: "desc test", name: "test")
+    assert_equal(@menu.save, false)
+  end
+
+  test "successful no description" do
+    @menu = Menu.new(rest_id:1, name: "desc test", price: 10)
+    assert @menu.save
+  end
+
+  test "successful get correct restaurant menu" do
+    @menu = Menu.new(rest_id:1, name: "desc", price: 10)
+    assert @menu.save
+    @resp = Menu.get_restaurant_menu(1)
+    assert_equal(@resp.first, @menu)
+  end
+
+  test "not successful get restaurant menu 1" do
+    @menu = Menu.new(rest_id:2, name: "desc test", price: 10)
+    assert @menu.save
+    @resp = Menu.get_restaurant_menu(10)
+    assert_not_equal(@resp, @menu)
+  end
+
+  test "not successful get restaurant menu 2" do
+  	@customer = Customer.new(user_id:2, phone_number: 0000)
+    @menu = Menu.new(rest_id:3, name: "desc test 2", price: 10)
+    assert @menu.save
+    @resp = Menu.get_restaurant_menu(@customer.user_id)
+    assert_equal(@resp, false)
+  end
+
 end
