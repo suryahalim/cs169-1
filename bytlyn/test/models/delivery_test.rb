96b4f6348ebd8c08757326cd0aab05af6a07e248
Jessica Lie <jenadlie@berkeley.edu>
2015-11-24 01:39:22 -0800

    test delivery

--- bytlyn/test/models/delivery_test.rb
+++ bytlyn/test/models/delivery_test.rb
@@ -61,4 +61,61 @@ class DeliveryTest < ActiveSupport::TestCase
     arrays = Delivery.get_order(user_id: 2, rest_id: 1, version:0)
     assert_equal(arrays.size, 2)
   end
+
+  test "don't get history for customer cart" do
+    @delivery1 = Delivery.new(phone: "20203020", rest_id: 1, version: 0, user_id: 2, address: "jalan jeruk", total: 6, status: 1)
+    @delivery1.save
+    @delivery2 = Delivery.new(phone: "2020300", rest_id: 1, version: 1, user_id: 2, address: "jalan jeruk", total: 6, status: 2)
+    @delivery2.save
+    @delivery3 = Delivery.new(phone: "2020300", rest_id: 1, version: 2, user_id: 2, address: "jalan jeruk", total: 6, status: 3)
+    @delivery3.save
+    @delivery4 = Delivery.new(phone: "2020300", rest_id: 1, version: 3, user_id: 2, address: "jalan jeruk", total: 6, status: 4)
+    @delivery4.save
+    assert_equal(Delivery.get_customer_delivery([2]).size, 3)
+  end
+
+  test "don't get history for restaurant" do
+    @delivery1 = Delivery.new(phone: "20203020", rest_id: 1, version: 0, user_id: 2, address: "jalan jeruk", total: 6, status: 1)
+    @delivery1.save
+    @delivery2 = Delivery.new(phone: "2020300", rest_id: 1, version: 1, user_id: 2, address: "jalan jeruk", total: 6, status: 2)
+    @delivery2.save
+    @delivery3 = Delivery.new(phone: "2020300", rest_id: 1, version: 2, user_id: 2, address: "jalan jeruk", total: 6, status: 3)
+    @delivery3.save
+    @delivery4 = Delivery.new(phone: "2020300", rest_id: 1, version: 3, user_id: 2, address: "jalan jeruk", total: 6, status: 4)
+    @delivery4.save
+    assert_equal(Delivery.get_restaurant_delivery([1]).size, 3)
+  end
+
+  test "get history only status 4 customer" do
+    @delivery1 = Delivery.new(phone: "20203020", rest_id: 1, version: 0, user_id: 2, address: "jalan jeruk", total: 6, status: 1)
+    @delivery1.save
+    @delivery2 = Delivery.new(phone: "2020300", rest_id: 1, version: 1, user_id: 2, address: "jalan jeruk", total: 6, status: 2)
+    @delivery2.save
+    @delivery3 = Delivery.new(phone: "2020300", rest_id: 1, version: 2, user_id: 2, address: "jalan jeruk", total: 6, status: 3)
+    @delivery3.save
+    @delivery4 = Delivery.new(phone: "2020300", rest_id: 1, version: 3, user_id: 2, address: "jalan jeruk", total: 6, status: 4)
+    @delivery4.save
+    assert_equal(Delivery.get_customer_delivery_history([2]).size, 1)
+    assert_equal(Delivery.get_customer_delivery_history([2]).first, @delivery4)
+  end
+
+  test "get history only status 4 restaurant" do
+    @delivery1 = Delivery.new(phone: "20203020", rest_id: 1, version: 4, user_id: 2, address: "jalan jeruk", total: 6, status: 1)
+    @delivery1.save
+    @delivery2 = Delivery.new(phone: "2020300", rest_id: 1, version: 1, user_id: 2, address: "jalan jeruk", total: 6, status: 2)
+    @delivery2.save
+    @delivery3 = Delivery.new(phone: "2020300", rest_id: 1, version: 2, user_id: 2, address: "jalan jeruk", total: 6, status: 3)
+    @delivery3.save
+    @delivery4 = Delivery.new(phone: "2020300", rest_id: 1, version: 0, user_id: 2, address: "jalan jeruk", total: 6, status: 4)
+    @delivery4.save
+    assert_equal(Delivery.get_restaurant_delivery_history([1]).size, 1)
+    assert_equal(Delivery.get_restaurant_delivery_history([1]).first, {delivery: @delivery4, cart: [{:name=>"test1", :qty=>1}, {:name=>"test2", :qty=>1}]})
+  end
+
+  test "status string" do
+    assert_equal('Order Processed', Delivery.status_string(1))
+    assert_equal('Preparing Order', Delivery.status_string(2))
+    assert_equal('On The Way', Delivery.status_string(3))
+    assert_equal('Delivered', Delivery.status_string(4))
+    assert_equal('Unknown', Delivery.status_string(5))
 end
